ae4146c807c094a17358945b7f3702266cb471a4
mmeigooni <mmeigooni@gmail.com>
2017-03-24 18:14:03 -0500

    new exercises and tests

---
+++ tests/computeAreaOfARectangleTests.js
@@ -0,0 +1,9 @@
+
+describe("computeAreaOfARectangle", function() {
+  it("should return a number", function() {
+    expect(typeof computeAreaOfARectangle(7, 6)).toBe("number");
+  });
+  it("should return the area of a rectangle", function() {
+    expect(computeAreaOfARectangle(7, 6)).toBe(42);
+  });
+})
